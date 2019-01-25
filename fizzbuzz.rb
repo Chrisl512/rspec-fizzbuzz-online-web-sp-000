b50aa7152fb2534b3ed1e158a253e5584a31f902
Christopher Luna <chris@illusionlayervfx.com>
2019-01-25 01:44:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
-def fizzbuzz(int)
- if int % 3 == 0
-  "Fizz"
-elsif int % 5 == 0
-  "Buzz"
-elsif (int % 3 && int % 5) == 15
+#def fizzbuzz(int)
+# if int % 3 == 0
+#  "Fizz"
+#elsif int % 5 == 0
+#  "Buzz"
+elsif (int % 3 && int % 5 == 15
   "FizzBuzz"
  end
 end
